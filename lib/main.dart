b55c57d7cf4ca6009e8f4cc1625068bd26df6527
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-04-07 14:00:08 -0400

    feat(theme): Add custom flex_color_scheme theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:estudazz_main_code/theme/appTheme.dart';
 import 'package:estudazz_main_code/views/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -5,6 +6,9 @@ import 'package:get/get_navigation/get_navigation.dart';
 void main() {
   runApp(
     GetMaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: AppTheme.themeMode,
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     )
---
+++ lib/theme/appTheme.dart
@@ -0,0 +1,37 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract final class AppTheme {
+  static ThemeData lightTheme = FlexThemeData.light(
+    scheme: FlexScheme.shadOrange,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      useM2StyleDividerInM3: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+  );
+
+  static ThemeData darkTheme = FlexThemeData.dark(
+    scheme: FlexScheme.shadOrange,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      blendOnColors: true,
+      useM2StyleDividerInM3: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+  );
+  static final themeMode = ThemeMode.system;
+}
