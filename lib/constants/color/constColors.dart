7ca91613a4d00e81b03473d643cec5c670fa31f0
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-05-05 12:09:49 -0400

    feat(ia): Implement message input field with send functionality

--- lib/constants/color/constColors.dart
+++ lib/constants/color/constColors.dart
@@ -7,6 +7,7 @@ class ConstColors {
   static const Color yellowColor = Color(0xFFE0D101);
   static const Color greyColor = Color(0xFF525252);
   static const Color grey400Color = Color(0xFFBDBDBD);
+  static const Color grey900Color = Color(0xFF212121);
   static const Color blackColor = Color(0xFF0F0F0F);
   static const Color black54Color = Color(0x89000000);
   static const Color orangeColor = Color(0xFFED820E);
--- lib/views/ia/iaPage.dart
+++ lib/views/ia/iaPage.dart
@@ -1,4 +1,5 @@
 import 'package:estudazz_main_code/components/custom/customAppBar.dart';
+import 'package:estudazz_main_code/constants/color/constColors.dart';
 import 'package:flutter/material.dart';
 
 class IaPage extends StatefulWidget {
@@ -9,11 +10,52 @@ class IaPage extends StatefulWidget {
 }
 
 class _IaPageState extends State<IaPage> {
+  final TextEditingController _messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(titleAppBar: 'InteA', showSettingsIAIcon: true),
-      body: Column(children: []),
+      appBar: CustomAppBar(
+        titleAppBar: 'Inteligência Artificial',
+        showSettingsIAIcon: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Container()),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            color: Colors.black,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _messageController,
+                    style: TextStyle(color: ConstColors.whiteColor),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: ConstColors.grey900Color,
+                      hintText: 'Digite sua mensagem',
+                      hintStyle: TextStyle(color: ConstColors.greyColor),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                IconButton(
+                  icon: Icon(Icons.send, color: ConstColors.whiteColor),
+                  onPressed: () {
+                    print('Mensagem enviada: ${_messageController.text}');
+                    _messageController.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
