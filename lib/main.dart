62fea6d431d159e7f57f3109b235660ed40b9d1d
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-05-26 17:12:09 -0400

    feat(main): load ENV from .env using flutter_dotenv and add .env to assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:estudazz_main_code/routes/appRoutes.dart';
 import 'package:estudazz_main_code/theme/appTheme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
@@ -11,8 +12,17 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Tenta acessar o arquivo .env
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print("Erro ao carregar o arquivo .env: $e");
+  }
+
   runApp(
     GetMaterialApp(
+
+      // -- Configurações de Localização --
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
@@ -23,10 +33,13 @@ Future<void> main() async {
       ],
       locale: Locale('pt', 'BR'), 
       
+      // -- Configurações de Tema --
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: AppTheme.themeMode,
       debugShowCheckedModeBanner: false,
+
+      // -- Configurações de Rotas --
       initialRoute: AppRoutes.homePage,
       getPages: AppRoutes.routes,
     ),
