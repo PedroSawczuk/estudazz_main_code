97f53a8ed69bb3f38723c6b70923d43a0ddd7f0d
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-05-26 18:06:38 -0400

    feat(ai): add initial structure for AiGeminiServices with API key documentation

---
+++ lib/services/ai/aiGeminiServices.dart
@@ -0,0 +1,12 @@
+/* 
+  Toda a parte do serviço da API do Gemini está aqui a baixo
+
+  Caso queira rodar localmente, você precisa de uma chave de API do Gemini.
+
+  Você consegue pegar ela aqui: https://aistudio.google.com/apikey
+*/
+
+class AiGeminiServices {
+
+  static const String _apiKey = 
+}
