dcf93930870975279f5d247b54de811dc3079909
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-06-25 17:11:45 -0400

    feat(constColors): reorganize color constants for improved structure and readability

--- lib/constants/color/constColors.dart
+++ lib/constants/color/constColors.dart
@@ -4,25 +4,30 @@ class ConstColors {
   static const Color whiteColor = Color(0xFFF7F3F2);
   static const Color white54Color = Colors.white54;
 
-  static const Color redColor = Color(0xFFFF280C);
-  static const Color darkRedColor = Color(0xFFB90F0F);
-  static const Color greenColor = Colors.green;
-  static const Color blueColor = Colors.blue;
-  static const Color lightBlueColor = Color(0xFF4F7DFF);
-  static const Color indigoColor = Colors.indigo;
-  static const Color tealColor = Colors.teal;
-  static const Color yellowColor = Color(0xFFE0D101);
-  static const Color greyColor = Color(0xFF525252);
-  static const Color grey200Color = Color(0xFFEEEEEE);
-  static const Color grey300Color = Color(0xFFE0E0E0);
-  static const Color grey400Color = Color(0xFFBDBDBD);
-  static const Color grey900Color = Color(0xFF212121);
   static const Color blackColor = Color(0xFF0F0F0F);
   static const Color black54Color = Color(0x89000000);
   static const Color black87Color = Color(0xDE000000);
 
+  static const Color grey900Color = Color(0xFF212121);
+  static const Color greyColor = Color(0xFF525252);
+  static const Color grey400Color = Color(0xFFBDBDBD);
+  static const Color grey300Color = Color(0xFFE0E0E0);
+  static const Color grey200Color = Color(0xFFEEEEEE);
+
+  static const Color redColor = Color(0xFFFF280C);
+  static const Color darkRedColor = Color(0xFFB90F0F);
+
   static const Color orangeColor = Color(0xFFED820E);
 
-  static const Color accentPurpleColor = Colors.deepPurpleAccent;
+  static const Color yellowColor = Color(0xFFE0D101);
+
+  static const Color greenColor = Colors.green;
+  static const Color tealColor = Colors.teal;
+
+  static const Color blueColor = Colors.blue;
+  static const Color lightBlueColor = Color(0xFF4F7DFF);
+  static const Color indigoColor = Colors.indigo;
+
   static const Color purpleColor = Colors.deepPurple;
+  static const Color accentPurpleColor = Colors.deepPurpleAccent;
 }
