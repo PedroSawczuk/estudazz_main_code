0aff61a295f44061a1cc2995a3f018794266206e
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-06-25 14:54:51 -0400

    feat(main_test): add initial test for basic arithmetic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:estudazz_main_code/components/custom/customSnackBar.dart';
+import 'package:estudazz_main_code/constants/color/constColors.dart';
 import 'package:estudazz_main_code/firebase_options.dart';
 import 'package:estudazz_main_code/routes/appRoutes.dart';
 import 'package:estudazz_main_code/theme/appTheme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:estudazz_main_code/constants/color/constColors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
