23d0b3bb2621319e204a5406dd682b1d52e5d924
PedroSawczuk <pedrosawczuk53@gmail.com>
2025-08-14 19:48:55 -0400

    feat: enhance long press functionality to copy text and show feedback in AIResponseMessage and UserInputMessage

--- lib/components/ia/aiResponseMessage.dart
+++ lib/components/ia/aiResponseMessage.dart
@@ -1,5 +1,7 @@
 import 'package:estudazz_main_code/constants/color/constColors.dart';
+import 'package:estudazz_main_code/components/custom/customSnackBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 class AIResponseMessage extends StatelessWidget {
@@ -11,33 +13,46 @@ class AIResponseMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerLeft,
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          color: ConstColors.grey300Color,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: MarkdownBody(
-          data: text,
-          styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-            p: TextStyle(
-              color: ConstColors.black87Color,
-              fontSize: 16,
-              height: 1.5,
-              fontWeight: FontWeight.w400,
-            ),
-            a: TextStyle(
-              color: ConstColors.blueColor,
-              decoration: TextDecoration.underline,
-              fontWeight: FontWeight.w600,
-            ),
-            code: TextStyle(
-              color: ConstColors.whiteColor,
-              backgroundColor: ConstColors.grey900Color,
-              fontFamily: 'SourceCodePro',
-              fontSize: 14,
-              fontWeight: FontWeight.w600,
+      child: GestureDetector(
+        onLongPress: () async {
+          await Clipboard.setData(ClipboardData(text: text));
+          CustomSnackBar.show(
+            title: 'Copiado!',
+            message:
+                'O conteúdo da mensagem foi copiado para a área de transferência.',
+            backgroundColor: ConstColors.orangeColor,
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: ConstColors.grey300Color,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: MarkdownBody(
+            data: text,
+            styleSheet: MarkdownStyleSheet.fromTheme(
+              Theme.of(context),
+            ).copyWith(
+              p: TextStyle(
+                color: ConstColors.black87Color,
+                fontSize: 16,
+                height: 1.5,
+                fontWeight: FontWeight.w400,
+              ),
+              a: TextStyle(
+                color: ConstColors.blueColor,
+                decoration: TextDecoration.underline,
+                fontWeight: FontWeight.w600,
+              ),
+              code: TextStyle(
+                color: ConstColors.whiteColor,
+                backgroundColor: ConstColors.grey900Color,
+                fontFamily: 'SourceCodePro',
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
             ),
           ),
         ),
--- lib/components/ia/userInputMessage.dart
+++ lib/components/ia/userInputMessage.dart
@@ -1,5 +1,7 @@
+import 'package:estudazz_main_code/components/custom/customSnackBar.dart';
 import 'package:estudazz_main_code/constants/color/constColors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 class UserInputMessage extends StatelessWidget {
@@ -11,17 +13,28 @@ class UserInputMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          color: ConstColors.orangeColor,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: MarkdownBody(
-          data: text,
-          styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-            p: TextStyle(color: ConstColors.whiteColor),
+      child: GestureDetector(
+        onLongPress: () async {
+          await Clipboard.setData(ClipboardData(text: text));
+          CustomSnackBar.show(
+            title: 'Copiado!',
+            message:
+                'O conteúdo da mensagem foi copiado para a área de transferência.',
+            backgroundColor: ConstColors.orangeColor,
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: ConstColors.orangeColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: MarkdownBody(
+            data: text,
+            styleSheet: MarkdownStyleSheet.fromTheme(
+              Theme.of(context),
+            ).copyWith(p: TextStyle(color: ConstColors.whiteColor)),
           ),
         ),
       ),
